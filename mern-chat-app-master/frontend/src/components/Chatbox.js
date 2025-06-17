9c41842a498a4f252f90410dbd93f116256e7ae3
Aryansingh2434 <167241406+Aryansingh2434@users.noreply.github.com>
2025-06-17 10:45:42 +0530

    Update Chatbox.js

--- mern-chat-app-master/frontend/src/components/Chatbox.js
+++ mern-chat-app-master/frontend/src/components/Chatbox.js
@@ -7,18 +7,23 @@ const Chatbox = ({ fetchAgain, setFetchAgain }) => {
   const { selectedChat } = ChatState();
 
   return (
-    <Box
-      d={{ base: selectedChat ? "flex" : "none", md: "flex" }}
-      alignItems="center"
-      flexDir="column"
-      p={3}
-      bg="white"
-      w={{ base: "100%", md: "68%" }}
-      borderRadius="lg"
-      borderWidth="1px"
-    >
-      <SingleChat fetchAgain={fetchAgain} setFetchAgain={setFetchAgain} />
-    </Box>
+  <Box
+  display={{ base: selectedChat ? "flex" : "none", md: "flex" }}
+  alignItems="center"
+  justifyContent="space-between"
+  flexDirection="column"
+  padding={4}
+  backgroundColor="white"
+  width={{ base: "100%", md: "70%" }}
+  borderRadius="2xl"
+  boxShadow="md"
+  border="1px solid"
+  borderColor="gray.200"
+  transition="all 0.3s ease-in-out"
+>
+  <SingleChat fetchAgain={fetchAgain} setFetchAgain={setFetchAgain} />
+</Box>
+
   );
 };
 
